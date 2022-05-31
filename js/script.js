79f316b8a93b7763f3191c040825f3e35f4e1155
Rostyslav <bovonenko@icloud.com>
2022-05-31 21:31:37 +0200

    input done

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 /* Задания на урок:
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 
@@ -24,12 +26,27 @@ const movieDB = {
     ]
 };
 
+
 const adv = document.querySelectorAll('.promo__adv img'),
       poster = document.querySelector('.promo__bg'),
       genre = poster.querySelector('.promo__genre'),
       wrapper = document.querySelector('.promo__interactive'),
       movieList = wrapper.querySelector('.promo__interactive-list'),
-      films = wrapper.querySelectorAll('.promo__interactive-item');
+      films = wrapper.querySelectorAll('.promo__interactive-item'),
+      add = document.querySelector('.add'),
+      input = add.querySelector('.adding__input'),
+      btn = add.querySelector('button');
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    let newFilm;
+    if (input.value.length > 21) {
+        newFilm = input.value.slice(0, 21) + '...';
+    }
+    movieDB.movies.push(newFilm);
+    input.value  = '';
+    console.log(movieDB.movies);
+});
 
 adv.forEach(item => {
     item.remove();
